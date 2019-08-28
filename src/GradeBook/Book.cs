8f2d4d10dbba44e87aa7a77ea11963996bcf7cbd
Joe <yogurmystery@gmail.com>
2019-08-27 19:27:29 -0500

    Moved statistics logic to book object method

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook{
@@ -8,6 +9,21 @@ namespace GradeBook{
     }
     public void AddGrade(double grade){
       grades.Add(grade);
+    }
+    public void ShowStatistics()
+    {
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+      foreach (var number in grades)
+      {
+        highGrade = Math.Max(number, highGrade);
+        lowGrade = Math.Min(number, lowGrade);
+        result += number;
+      }
+      Console.WriteLine($"The average grade is { result / grades.Count:N2}");
+      System.Console.WriteLine($"The highest grade was {highGrade}");
+      System.Console.WriteLine($"The lowest grade was {lowGrade}");
     }
     private List<double> grades;
     private string name;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,27 +7,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-
-            var book2 = new Book("Scooter's Book");
-            book2.AddGrade(90.1);
-
             var book = new Book("Chester's Book");
             book.AddGrade(89.1);
-
-            var grades = new List<double>() { 12.3, 44.6, 83.456, 44, 623.444, 9.0000002343 };
-            grades.Add(69);
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach(var number in grades){
-              highGrade = Math.Max(number, highGrade);
-              lowGrade = Math.Min(number, lowGrade);
-              result += number;
-            }
-            Console.WriteLine($"The average grade is { result / grades.Count:N2}");
-            System.Console.WriteLine($"The highest grade was {highGrade}");
-            System.Console.WriteLine($"The lowest grade was {lowGrade}");
+            book.AddGrade(90.5);
+            book.AddGrade(66.5);
+            book.ShowStatistics();
             
             if(args.Length > 0)
             {
